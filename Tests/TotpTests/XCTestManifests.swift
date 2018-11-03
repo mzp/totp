3c92af172e6bfa76934b76617469a24310fcce5e
mzp <mzpppp@gmail.com>
2018-11-03 19:14:05 +0900

    :lipstick: fix manifest

--- Tests/TotpTests/XCTestManifests.swift
+++ Tests/TotpTests/XCTestManifests.swift
@@ -3,7 +3,9 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(TotpTests.allTests)
+        testCase(HOTPGeneratorTests.allTests),
+        testCase(TOTPGeneratorTests.allTests),
+        testCase(UInt64_BytesTests.allTests)
     ]
 }
 #endif
